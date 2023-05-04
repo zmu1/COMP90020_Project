ac57ce268ff1ec17837ff1110059524cb86c3cb7
jackson-hu1279 <68998854+jackson-hu1279@users.noreply.github.com>
2023-05-04 12:05:57 +1000

    Fix incomplete socket bytes received

--- CommHelper.py
+++ CommHelper.py
@@ -1,20 +1,42 @@
 import pickle
 import struct
 
-def send_socket_msg(conn, type, content=None):
+
+def send_socket_msg(conn, type, content=None, debug=False):
     msg = {'type': type, 'content': content}
 
     packet = pickle.dumps(msg)
-    length_in_4_bytes = struct.pack('I', len(packet))
-    packet = length_in_4_bytes + packet
+    size = len(packet)
+    length_in_4_bytes = struct.pack('I', size)
 
-    conn.send(packet)
+    conn.sendall(length_in_4_bytes)
+    conn.sendall(packet)
+
+    if debug:
+        print("\n###############")
+        print("Sent header size:", size)
+        print("Sent packet size:", len(packet))
+        print("Sent packet type:", type)
+        print("###############\n")
 
 
-def recv_socket_msg(conn):
+def recv_socket_msg(conn, debug=False):
     length_in_4_bytes = conn.recv(4)
     size = struct.unpack('I', length_in_4_bytes)
     size = size[0]
-    data = conn.recv(size)
+
+    # Keep reading the incoming bytes
+    # Until all bytes length required are received
+    data = bytearray(size)
+    pos = 0
+    while pos < size:
+        n = conn.recv_into(memoryview(data)[pos:])
+        pos += n
+
+    if debug:
+        print("\n*****************")
+        print("Received header size:", size)
+        print("Received packet size:", len(data))
+        print("*****************\n")
 
     return pickle.loads(data)
